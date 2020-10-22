47917fb55edd66236a0f6bc390e54942ecf76f62
unknown <ksbohyun@naver.com>
2020-10-22 21:46:49 +0900

    firebase 셋업

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXuNNbHYg_HHmBTBvvCkQNUxh7mvhbYQY",
+  authDomain: "twitter-clone-86aef.firebaseapp.com",
+  databaseURL: "https://twitter-clone-86aef.firebaseio.com",
+  projectId: "twitter-clone-86aef",
+  storageBucket: "twitter-clone-86aef.appspot.com",
+  messagingSenderId: "906381663356",
+  appId: "1:906381663356:web:c192716e03237bfcd2bdf9",
+};
+
+export default firebase.initializeApp(firebaseConfig);
