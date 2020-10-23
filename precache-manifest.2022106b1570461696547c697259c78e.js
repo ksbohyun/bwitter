7a7e3375bfc46573ecadce4b36fa53db028f580e
unknown <ksbohyun@naver.com>
2020-10-23 18:32:28 +0900

    Updates

---
+++ precache-manifest.2022106b1570461696547c697259c78e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1de9ab3ff1f83242785cb97929b0ed",
+    "url": "/bwitter/index.html"
+  },
+  {
+    "revision": "09f8ea42c884d6fbca41",
+    "url": "/bwitter/static/css/main.c89ea8f9.chunk.css"
+  },
+  {
+    "revision": "3c6f1296e96f097aba0f",
+    "url": "/bwitter/static/js/2.f6d11186.chunk.js"
+  },
+  {
+    "revision": "954d88908e6b1f345323e3bc51916644",
+    "url": "/bwitter/static/js/2.f6d11186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09f8ea42c884d6fbca41",
+    "url": "/bwitter/static/js/main.bcb5c5d6.chunk.js"
+  },
+  {
+    "revision": "30552d965a736ef713e8",
+    "url": "/bwitter/static/js/runtime-main.47b06f03.js"
+  }
+]);
